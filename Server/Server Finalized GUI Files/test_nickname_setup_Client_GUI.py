0322866f3c410b4b04d849ef1baa95a4858008ab
Lewey <79818000+leweyccac@users.noreply.github.com>
2023-04-30 09:39:02 -0600

    Update test_nickname_setup_Client_GUI.py

--- Server/Server Finalized GUI Files/test_nickname_setup_Client_GUI.py
+++ Server/Server Finalized GUI Files/test_nickname_setup_Client_GUI.py
@@ -12,18 +12,15 @@ class TestUiClient(unittest.TestCase):
         self.ui.setupUi(self.form)
 
     def test_nickname_setup_successful(self):
-        # Set up the input fields
         self.ui.nicknameTxt.setText("TestUser")
 
         self.ui.nickname = self.ui.nicknameTxt.text()
 
         with patch.object(
             Ui_Client, "start_thread", return_value=None
-        ):  # Mock start_thread method
-            # Simulate a successful nickname setup
+        ):
             self.ui.nickname_setup()
 
-        # Check that the UI elements are in the correct state
         self.assertFalse(self.ui.nicknameTxt.isEnabled())
         self.assertFalse(self.ui.nicknameBtn.isEnabled())
         self.assertTrue(self.ui.msgTxt.isEnabled())
@@ -36,11 +33,8 @@ class TestUiClient(unittest.TestCase):
         self.assertIn("Client starting.\n", self.ui.logTxt.toPlainText())
 
     def test_nickname_setup_unsuccessful(self):
-        # Set up the input fields
         self.ui.nicknameTxt.setText("")
         self.ui.nickname = self.ui.nicknameTxt.text()
-
-        # Simulate an unsuccessful nickname setup
         self.ui.nickname_setup()
         self.assertIn(
             "Incorrect nickname. Nickname can't be white space.\n",
